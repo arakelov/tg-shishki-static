d566eeeec9d7b957adf973fff68a16e39f44a306
tekitus <arakelovolod@gmail.com>
2024-05-25 23:05:58 +0000

    bottom navigation

--- src/components/BottomMenu/BottomMenu.jsx
+++ src/components/BottomMenu/BottomMenu.jsx
@@ -5,7 +5,7 @@ import FlashOnIcon from '@mui/icons-material/FlashOn';
 import TaskIcon from '@mui/icons-material/Assignment';
 import './BottomMenu.css'
 
-export default function SimpleBottomNavigation() {
+const SimpleBottomNavigation = () => {
   const [value, setValue] = useState(0);
 
   return (
@@ -15,11 +15,13 @@ export default function SimpleBottomNavigation() {
         setValue(newValue);
       }}
       showLabels
-      className="bottomnavbar"
+      className={'bottomnavbar'}
     >
       <BottomNavigationAction label="Forest" icon={<ParkIcon />} />
       <BottomNavigationAction label="Boost" icon={<FlashOnIcon />} />
       <BottomNavigationAction label="Tasks" icon={<TaskIcon />} />
     </BottomNavigation>
   );
-}
+};
+
+export default SimpleBottomNavigation
