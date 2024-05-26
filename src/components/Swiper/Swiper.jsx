734fd2fed43e66518f54f61ad08f7f12e9324f60
tekitus <arakelovolod@gmail.com>
2024-05-26 21:10:29 +0000

    pages

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,11 +1,19 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './Swiper.css'
 import logo from '../../images/bush.png';
+import img1 from '../../images/picea_abies.png';
+import img2 from '../../images/pinus_nigra.png';
+import img3 from '../../images/pinus_pinea.png';
+import img4 from '../../images/pseudotsuga.png';
 
 import { Pagination } from 'swiper/modules';
 
@@ -15,19 +23,66 @@ export default () => {
         dynamicBullets: true,
       }}
       modules={[Pagination]}>
-      <SwiperSlide><div class="green-rectangle">
-    <div class="brown-rectangle"><p>Shishka 1</p></div>
-  </div></SwiperSlide>
-      <SwiperSlide><div class="green-rectangle">
-    <div class="brown-rectangle"><p>Shishka 2</p></div>
-  </div></SwiperSlide>
-      <SwiperSlide><div class="green-rectangle">
-    <div class="brown-rectangle"><p>Shishka 3</p></div>
-  </div></SwiperSlide>
-      <SwiperSlide><div class="green-rectangle">
-    <div class="brown-rectangle"><p>Shishka 4</p></div>
-  </div></SwiperSlide>
-      <SwiperSlide><img src={logo} /></SwiperSlide>
+      <SwiperSlide>
+      <div  class="card shop-products">
+                    <div  class="card-image">
+                    <img src={logo} />
+                    </div>
+                    <div  class="card-action accent-3 white-text">
+                    <p>
+                        <span>Kust Obyknovenny</span>
+                    </p>
+                    </div>
+                </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div  class="card shop-products">
+                    <div  class="card-image">
+                    <img src={img1} />
+                    </div>
+                    <div  class="card-action accent-3 white-text">
+                    <p>
+                        <span>Pinea Abies</span>
+                    </p>
+                    </div>
+                </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div  class="card shop-products">
+                    <div  class="card-image">
+                    <img src={img2} />
+                    </div>
+                    <div  class="card-action accent-3 white-text">
+                    <p>
+                        <span>Pinus Nigra</span>
+                    </p>
+                    </div>
+                </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div  class="card shop-products">
+                    <div  class="card-image">
+                    <img src={img3} />
+                    </div>
+                    <div  class="card-action accent-3 white-text">
+                    <p>
+                        <span>Pinus Pinea</span>
+                    </p>
+                    </div>
+                </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div  class="card shop-products">
+                    <div  class="card-image">
+                    <img src={img4} />
+                    </div>
+                    <div  class="card-action accent-3 white-text">
+                    <p>
+                        <span>Pseudotsuga</span>
+                    </p>
+                    </div>
+                </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
