7370869e8fe281f19dc30ff206125cf1b6b58191
tekitus <arakelovolod@gmail.com>
2024-05-26 01:06:45 +0000

    bottom navigation

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -6,10 +6,6 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import './Swiper.css'
 import logo from '../../images/bush.png';
-import img1 from '../../images/picea_abies.png';
-import img2 from '../../images/pinus_nigra.png';
-import img3 from '../../images/pinus_pinea.png';
-import img4 from '../../images/pseudotsuga.png';
 
 import { Pagination } from 'swiper/modules';
 
@@ -19,10 +15,18 @@ export default () => {
         dynamicBullets: true,
       }}
       modules={[Pagination]}>
-      <SwiperSlide><img src={img1} /></SwiperSlide>
-      <SwiperSlide><img src={img2} /></SwiperSlide>
-      <SwiperSlide><img src={img3} /></SwiperSlide>
-      <SwiperSlide><img src={img4} /></SwiperSlide>
+      <SwiperSlide><div class="green-rectangle">
+    <div class="brown-rectangle"><p>Shishka 1</p></div>
+  </div></SwiperSlide>
+      <SwiperSlide><div class="green-rectangle">
+    <div class="brown-rectangle"><p>Shishka 2</p></div>
+  </div></SwiperSlide>
+      <SwiperSlide><div class="green-rectangle">
+    <div class="brown-rectangle"><p>Shishka 3</p></div>
+  </div></SwiperSlide>
+      <SwiperSlide><div class="green-rectangle">
+    <div class="brown-rectangle"><p>Shishka 4</p></div>
+  </div></SwiperSlide>
       <SwiperSlide><img src={logo} /></SwiperSlide>
     </Swiper>
   );
