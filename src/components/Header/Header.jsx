db75efac20273022aabb8fe63c57036d994d23a9
tekitus <arakelovolod@gmail.com>
2024-07-07 10:35:40 +0000

    animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,28 @@ import './Header.css';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
+    let is_bot
+    let is_premium
+    if (user?.is_bot){
+        is_bot = "Yes"
+    } else {
+        is_bot = "No"
+    }
+
+    if (user?.is_premium){
+        is_premium = "Yes"
+    } else {
+        is_premium = "No"
+    }
 
     return (
         <div className={'header'}>
             {/* <Button onClick={onClose}>Close</Button> */}
             <span className={'username'}>
             <p>Welcome to SHISHKI, {user?.id};{user?.username};{user?.first_name};{user?.last_name}</p>
-            <p>Are you bot: {user?.is_bot}</p>
+            <p>Are you bot: {is_bot}</p>
             <p>Your language: {user?.language_code}</p>
-            <p>Have premium: {user?.is_premium}</p>
+            <p>Have premium: {is_premium}</p>
             </span>
         </div>
     );
